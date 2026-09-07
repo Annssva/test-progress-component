85925abf54799eef63b56c6d3c6fa2c41b97c6a3
annssva <sysoevaannaa@mail.ru>
2026-09-07 17:39:43 +0300

    test controls

--- js/app.js
+++ js/app.js
@@ -3,5 +3,18 @@ const progress = createProgress(
     75
 );
 
-progress.setAnimate(true);
-progress.setHidden(true);
+const valueInput = document.querySelector('#value-input');
+const animateInput = document.querySelector('#animate-input');
+const hideInput = document.querySelector('#hide-input');
+
+valueInput.addEventListener('input', () => {
+    progress.setValue(Number(valueInput.value));
+});
+
+animateInput.addEventListener('change', () => {
+    progress.setAnimate(animateInput.checked);
+});
+
+hideInput.addEventListener('change', () => {
+    progress.setHidden(hideInput.checked);
+});
